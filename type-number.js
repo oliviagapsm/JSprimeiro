655e501dd002a4aee06be6df004062ed9aba7ec1
oliviagapsm <olivia.gonzalezmacedo@gmail.com>
2022-03-22 09:56:11 -0300

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,23 @@
+// tipo Number
+
+const meuNumro = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+// Nan -> Not a Number 
+
+const alura = "Alura";
+console.log(alura * primeiroNumero);
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Olívia";
+const minusculo = meuNome.toLowerCase();
+console.log(citacao);
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+console.log(minusculo)
