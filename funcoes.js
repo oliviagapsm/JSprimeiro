25d5c09702f506c3c770247a708d948379d1d207
oliviagapsm <olivia.gonzalezmacedo@gmail.com>
2022-03-22 15:25:59 -0300

    comparacoes, erros, funcoes, ternario e template-string

---
+++ funcoes.js
@@ -0,0 +1,22 @@
+// let x = "";
+// console.log(x)
+// x = "oi";
+
+//1) declara função
+                      //string
+function imprimeTexto(texto){
+    console.log(texto)
+}
+
+// 2) execta a função (1 ou + vezes)
+
+imprimeTexto(soma());
+// imprimeTexto("outro texto")
+
+// três formas de escrever
+
+function soma(){
+    return 2 + 2; 
+}
+
+//console.log(soma())
---
+++ template-strings.js
@@ -0,0 +1,20 @@
+const nome = "Olívia";
+const idade = 2021 - 1998;
+const cidadeNascimento = "Brasília";
+
+// const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+
+console.log(apresentacao);
+
+a = []
+b = 20
+c = true 
+d = ''
+
+if (a == 0) {
+    console.log('Verdade')
+} else {
+    console.log('Falso')
+}
